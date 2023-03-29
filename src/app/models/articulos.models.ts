55cbd73f1d5dc9f80226102b179c688179a5ae1b
Carlos Menares <menaresdesarrollo@gmail.com>
2023-03-29 12:02:17 -0300

    Agregar articulos y mostrarlos

--- src/app/models/articulos.models.ts
+++ src/app/models/articulos.models.ts
@@ -1,8 +1,8 @@
-class Articulo{
+export default class Articulo{
     codigo: number;
     descripcion: string;
     precio: number;
-    
+
     constructor(codigo: number, descripcion: string, precio: number){
         this.codigo = codigo;
         this.descripcion = descripcion;
